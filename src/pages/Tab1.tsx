8c78cf3e6fee14c8353537d3cbabcaf4c487ef0a
Dwi Saputra <dwisaputra204@gmail.com>
2025-04-25 15:52:49 +0800

    init project ionic

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,163 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonButton,
+  IonIcon,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonInput,
+  IonLabel,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/react";
+import "./Tab1.css";
+import { time, stop } from "ionicons/icons";
+import React, { useState } from "react";
+
+interface StopwatchState {
+  isRunning: boolean;
+  seconds: number;
+  intervalId: NodeJS.Timeout | null;
+}
 
 const Tab1: React.FC = () => {
+  const initialStopwatchStates: { [key: string]: StopwatchState } = {
+    "Digging Time": { isRunning: false, seconds: 0, intervalId: null },
+    "Swing-Time Loaded": { isRunning: false, seconds: 0, intervalId: null },
+    "Bucket-Dump Time": { isRunning: false, seconds: 0, intervalId: null },
+    "Swing-Time Empty": { isRunning: false, seconds: 0, intervalId: null },
+  };
+
+  const [stopwatchStates, setStopwatchStates] = useState(
+    initialStopwatchStates
+  );
+
+  const toggleStopwatch = (label: string) => {
+    // Hentikan semua interval yang aktif
+    Object.keys(stopwatchStates).forEach((key) => {
+      const interval = stopwatchStates[key].intervalId;
+      if (interval) {
+        clearInterval(interval);
+      }
+    });
+
+    // Buat interval baru hanya untuk stopwatch yang diklik
+    const newInterval = setInterval(() => {
+      setStopwatchStates((prev) => ({
+        ...prev,
+        [label]: {
+          ...prev[label],
+          seconds: prev[label].seconds + 1,
+        },
+      }));
+    }, 1000);
+
+    // Update semua stopwatch
+    setStopwatchStates((prevState) => {
+      const newState: typeof prevState = {};
+
+      for (const key in prevState) {
+        if (key === label) {
+          newState[key] = {
+            isRunning: true,
+            seconds: 0, // Reset hanya stopwatch yang dipilih
+            intervalId: newInterval,
+          };
+        } else {
+          newState[key] = {
+            ...prevState[key],
+            isRunning: false,
+            intervalId: null,
+          };
+        }
+      }
+
+      return newState;
+    });
+  };
+
+  const stopAllStopwatches = () => {
+    // Hentikan semua stopwatch
+    Object.keys(stopwatchStates).forEach((key) => {
+      const interval = stopwatchStates[key].intervalId;
+      if (interval) {
+        clearInterval(interval);
+      }
+    });
+
+    setStopwatchStates((prevState) => {
+      const newState: typeof prevState = {};
+      for (const key in prevState) {
+        newState[key] = {
+          ...prevState[key],
+          isRunning: false,
+          intervalId: null,
+        };
+      }
+      return newState;
+    });
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Cycle Counter</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Cycle Counter</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+        {Object.keys(stopwatchStates).map((label) => (
+          <IonItem key={label} lines="none">
+            <IonGrid>
+              <IonRow style={{ alignItems: "center" }}>
+                <IonCol size="8">
+                  <IonLabel position="stacked">{label}</IonLabel>
+                  <IonInput
+                    value={stopwatchStates[label].seconds}
+                    readonly
+                    placeholder="0"
+                  ></IonInput>
+                </IonCol>
+                <IonCol size="4" className="ion-text-end">
+                  <IonButton
+                    shape="round"
+                    onClick={() => toggleStopwatch(label)}
+                  >
+                    <IonIcon
+                      slot="icon-only"
+                      icon={stopwatchStates[label].isRunning ? undefined : time}
+                    ></IonIcon>
+                  </IonButton>
+                </IonCol>
+              </IonRow>
+            </IonGrid>
+          </IonItem>
+        ))}
+        <IonItem lines="none">
+          <IonGrid>
+            <IonRow style={{ alignItems: "center" }}>
+              <IonCol size="12">
+                <IonButton
+                  expand="block"
+                  color="danger"
+                  onClick={stopAllStopwatches}
+                >
+                  <IonIcon slot="start" icon={stop}></IonIcon>
+                  Stop
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonItem>
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,62 +1,160 @@
-import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg, IonActionSheet } from '@ionic/react';
-import { camera, trash, close } from 'ionicons/icons';
-import { usePhotoGallery, UserPhoto } from '../hooks/usePhotoGallery';
+import {
+  IonContent,
+  IonHeader,
+  IonButton,
+  IonIcon,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonInput,
+  IonLabel,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/react";
+import "./Tab2.css";
+import { time, stop } from "ionicons/icons";
+import React, { useState } from "react";
+
+interface StopwatchState {
+  isRunning: boolean;
+  seconds: number;
+  intervalId: NodeJS.Timeout | null;
+}
 
 const Tab2: React.FC = () => {
-  const { deletePhoto, photos, takePhoto } = usePhotoGallery();
-  const [photoToDelete, setPhotoToDelete] = useState<UserPhoto>();
+  const initialStopwatchStates: { [key: string]: StopwatchState } = {
+    "Hauling Time": { isRunning: false, seconds: 0, intervalId: null },
+  };
+
+  const [stopwatchStates, setStopwatchStates] = useState(
+    initialStopwatchStates
+  );
+
+  const toggleStopwatch = (label: string) => {
+    // Hentikan semua interval yang aktif
+    Object.keys(stopwatchStates).forEach((key) => {
+      const interval = stopwatchStates[key].intervalId;
+      if (interval) {
+        clearInterval(interval);
+      }
+    });
+
+    // Buat interval baru hanya untuk stopwatch yang diklik
+    const newInterval = setInterval(() => {
+      setStopwatchStates((prev) => ({
+        ...prev,
+        [label]: {
+          ...prev[label],
+          seconds: prev[label].seconds + 1,
+        },
+      }));
+    }, 1000);
+
+    // Update semua stopwatch
+    setStopwatchStates((prevState) => {
+      const newState: typeof prevState = {};
+
+      for (const key in prevState) {
+        if (key === label) {
+          newState[key] = {
+            isRunning: true,
+            seconds: 0, // Reset hanya stopwatch yang dipilih
+            intervalId: newInterval,
+          };
+        } else {
+          newState[key] = {
+            ...prevState[key],
+            isRunning: false,
+            intervalId: null,
+          };
+        }
+      }
+
+      return newState;
+    });
+  };
+
+  const stopAllStopwatches = () => {
+    // Hentikan semua stopwatch
+    Object.keys(stopwatchStates).forEach((key) => {
+      const interval = stopwatchStates[key].intervalId;
+      if (interval) {
+        clearInterval(interval);
+      }
+    });
+
+    setStopwatchStates((prevState) => {
+      const newState: typeof prevState = {};
+      for (const key in prevState) {
+        newState[key] = {
+          ...prevState[key],
+          isRunning: false,
+          intervalId: null,
+        };
+      }
+      return newState;
+    });
+  };
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Photo Gallery</IonTitle>
+          <IonTitle>Cycle Counter</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-      <IonHeader collapse="condense">
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Photo Gallery</IonTitle>
+            <IonTitle size="large">Cycle Counter</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonGrid>
-          <IonRow>
-            {photos.map((photo, index) => (
-              <IonCol size="6" key={index}>
-                <IonImg onClick={() => setPhotoToDelete(photo)} src={photo.webviewPath} />
+
+        {Object.keys(stopwatchStates).map((label) => (
+          <IonItem key={label} lines="none">
+            <IonGrid>
+              <IonRow style={{ alignItems: "center" }}>
+                <IonCol size="8">
+                  <IonLabel position="stacked">{label}</IonLabel>
+                  <IonInput
+                    value={stopwatchStates[label].seconds}
+                    readonly
+                    placeholder="0"
+                  ></IonInput>
+                </IonCol>
+                <IonCol size="4" className="ion-text-end">
+                  <IonButton
+                    shape="round"
+                    onClick={() => toggleStopwatch(label)}
+                  >
+                    <IonIcon
+                      slot="icon-only"
+                      icon={stopwatchStates[label].isRunning ? undefined : time}
+                    ></IonIcon>
+                  </IonButton>
+                </IonCol>
+              </IonRow>
+            </IonGrid>
+          </IonItem>
+        ))}
+        <IonItem lines="none">
+          <IonGrid>
+            <IonRow style={{ alignItems: "center" }}>
+              <IonCol size="12">
+                <IonButton
+                  expand="block"
+                  color="danger"
+                  onClick={stopAllStopwatches}
+                >
+                  <IonIcon slot="start" icon={stop}></IonIcon>
+                  Stop
+                </IonButton>
               </IonCol>
-            ))}
-          </IonRow>
-        </IonGrid>
-
-        <IonFab vertical="bottom" horizontal="center" slot="fixed">
-          <IonFabButton onClick={() => takePhoto()}>
-            <IonIcon icon={camera}></IonIcon>
-          </IonFabButton>
-        </IonFab>
-
-        <IonActionSheet
-          isOpen={!!photoToDelete}
-          buttons={[{
-            text: 'Delete',
-            role: 'destructive',
-            icon: trash,
-            handler: () => {
-              if (photoToDelete) {
-                deletePhoto(photoToDelete);
-                setPhotoToDelete(undefined);
-              }
-            }
-          }, {
-            text: 'Cancel',
-            icon: close,
-            role: 'cancel'
-          }]}
-          onDidDismiss={() => setPhotoToDelete(undefined)}
-        />
-
-
+            </IonRow>
+          </IonGrid>
+        </IonItem>
       </IonContent>
     </IonPage>
   );
