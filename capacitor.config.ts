eba91ad0fcdd8bc64a32d7eb0b7a4d135fd64156
Dwi Saputra <dwisaputra204@gmail.com>
2025-04-27 09:36:20 +0800

    Update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "io.ionic.demo.pg.react",
+  appId: "com.dwisaputra.cyclecounter",
   appName: "Cycle Counter",
   webDir: "dist",
   server: {
